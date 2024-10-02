c495bf408d62800f120cfb1de8ed4b9c8af6f1e6
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2024-10-02 14:58:04 -0500

    Reapply "Adding Lars' 2p2h dials"
    This reverts commit f6a8480a172d96850062c4a9afd27b9e5cdc68d8.

--- src/nusystematics/systproviders/GENIEReWeightEngineConfig.cc
+++ src/nusystematics/systproviders/GENIEReWeightEngineConfig.cc
@@ -293,7 +293,10 @@ ConfigureMECWeightEngine(SystMetaData const &MECmd,
         kXSecTwkDial_NormEMMEC,
         kXSecTwkDial_FracPN_CCMEC,
         kXSecTwkDial_FracDelta_CCMEC,
-        kXSecTwkDial_XSecShape_CCMEC
+        kXSecTwkDial_XSecShape_CCMEC,
+        kXSecTwkDial_XSecShape_CCMEC_Empirical,
+        kXSecTwkDial_XSecShape_CCMEC_Martini,
+        kXSecTwkDial_EnergyDependence_CCMEC
       },
       "xsec_mec", []() { return new GReWeightXSecMEC; }, UseFullHERG, param_map);
 
