a2bc58b05e2770d7e71faa83145c69798dd28123
Nathaniel <nathanielerowe@uchicago.edu>
2025-11-05 11:16:25 -0600

    Make namespace configurable

--- src/nusystematics/systproviders/ZExpPCAWeighter_tool.cc
+++ src/nusystematics/systproviders/ZExpPCAWeighter_tool.cc
@@ -30,6 +30,12 @@ using namespace systtools;
 using namespace std;
 
 // global variables
+//
+Eigen::Array4d central_values_a_from_genie = {2.30, -0.60, -3.80, 2.30};
+Eigen::Array4d central_values_afrom_errors_genie_code = {0.14, 0.67, 1,
+                                                         0.75}; //-----in genie
+Eigen::Array4d central_values_afrom_errors_genie = {0.13, 1, 2.5, 2.7};
+
 namespace PRD_93_113015 {
 Eigen::Vector4d a_14_cv{2.3, -0.6, -3.8, 2.3};
 Eigen::Vector4d a_14_errors{0.13, 1, 2.5, 2.7};
@@ -37,10 +43,6 @@ Eigen::MatrixXd Covariance_Matrix{{0.0169, 0.0455, -0.22035, 0.214461},
                                   {0.0455, 1., -2.245, 0.9909},
                                   {-0.22035, -2.245, 6.25, -4.62375},
                                   {0.214461, 0.9909, -4.62375, 7.29}};
-Eigen::Array4d central_values_a_from_genie = {2.30, -0.60, -3.80, 2.30};
-Eigen::Array4d central_values_afrom_errors_genie_code = {0.14, 0.67, 1,
-                                                         0.75}; //-----in genie
-Eigen::Array4d central_values_afrom_errors_genie = {0.13, 1, 2.5, 2.7};
 } // namespace PRD_93_113015
 
 namespace Nature_614_102522 {
@@ -50,10 +52,6 @@ Eigen::MatrixXd Covariance_Matrix{{0.0961, 0.002604, -0.54777, 0.403},
                                   {0.002604, 0.49, -0.4256, -1.365},
                                   {-0.54777, -0.4256, 3.61, -1.2825},
                                   {0.403, -1.365, -1.2825, 6.25}};
-Eigen::Array4d central_values_a_from_genie = {2.30, -0.60, -3.80, 2.30};
-Eigen::Array4d central_values_afrom_errors_genie_code = {0.14, 0.67, 1,
-                                                         0.75}; //-----in genie
-Eigen::Array4d central_values_afrom_errors_genie = {0.13, 1, 2.5, 2.7};
 } // namespace Nature_614_102522
 
 Eigen::MatrixXd
@@ -81,12 +79,12 @@ Eigen::VectorXd ChangeBasisBParams(std::vector<double> const &BVals,
 }
 
 // Function to rescale the a parameters for GENIE ReWeight
-Eigen::Array4d ScaleAparamsforGenie(Eigen::VectorXd &avals) {
+Eigen::Array4d ScaleAparamsforGenie(Eigen::Vector4d a_14_cv, Eigen::VectorXd &avals) {
   Eigen::VectorXd avalues_for_genie =
-      (((Nature_614_102522::a_14_cv + avals).array() /
-        Nature_614_102522::central_values_a_from_genie.array()) -
+      (((a_14_cv + avals).array() /
+        central_values_a_from_genie.array()) -
        1.0) /
-      Nature_614_102522::central_values_afrom_errors_genie_code.array();
+      central_values_afrom_errors_genie_code.array();
   return avalues_for_genie;
 }
 
@@ -140,8 +138,26 @@ SystMetaData ZExpPCAWeighter::BuildSystMetaData(fhicl::ParameterSet const &ps,
 bool ZExpPCAWeighter::SetupResponseCalculator(
     fhicl::ParameterSet const &tool_options) {
   verbosity_level = tool_options.get<int>("verbosity_level", 0);
-  std::string RwtoPub = tool_options.get<std::string>("RWtoPub", "PRD_93_113015");
-  std::cout << "Found RwtoPub: " << RwtoPub << std::endl;
+  std::string RWtoPub = tool_options.get<std::string>("RWtoPub", "PRD_93_113015");
+  std::cout << "Found RwtoPub: " << RWtoPub << std::endl;
+
+  Eigen::Vector4d a_14_cv; 
+  Eigen::Vector4d a_14_errors;
+  Eigen::MatrixXd Covariance_Matrix;
+
+  if(RWtoPub == "Nature_614_102522"){
+    a_14_cv = Nature_614_102522::a_14_cv;
+    a_14_errors = Nature_614_102522::a_14_errors;
+    Covariance_Matrix = Nature_614_102522::Covariance_Matrix;
+  }
+  else if(RWtoPub == "PRD_93_113015"){
+    a_14_cv = PRD_93_113015::a_14_cv;
+    a_14_errors = PRD_93_113015::a_14_errors;
+    Covariance_Matrix = PRD_93_113015::Covariance_Matrix;
+  }
+  else{
+    std::cout << RWtoPub << " Not available for RWtoPub" << std::endl; 
+  }
 
   // grab the pre-parsed param headers object
   SystMetaData const &md = GetSystMetaData();
@@ -204,10 +220,10 @@ bool ZExpPCAWeighter::SetupResponseCalculator(
       // md and the rotation function
       Eigen::VectorXd a_variations = ChangeBasisBParams(
           bvariations,
-          GetPCAFromCovarianceMatrix(Nature_614_102522::Covariance_Matrix));
+          GetPCAFromCovarianceMatrix(Covariance_Matrix));
 
       // Now use the b_variations to get the a values
-      auto myaparameters = ScaleAparamsforGenie(a_variations);
+      auto myaparameters = ScaleAparamsforGenie(a_14_cv, a_variations);
 
       if (verbosity_level > 2) {
         std::cout << "b[" << i << "] = " << v << std::endl;
