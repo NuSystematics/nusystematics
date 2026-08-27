c3c529b8de1410593f948825feefadbcc1ecc8ff
zihaolin2000 <zlin22@ur.rochester.edu>
2026-08-27 11:51:37 -0500

    Add ValenciaExc2p2hReweighter as a tool type

--- src/nusystematics/utility/make_instance.hh
+++ src/nusystematics/utility/make_instance.hh
@@ -25,6 +25,7 @@
 #include "nusystematics/systproviders/FSIReweightMult_tool.hh"
 #include "nusystematics/systproviders/WSReweight_tool.hh"
 #include "nusystematics/systproviders/MECq0q3InterpWeighting_tool.hh"
+#include "nusystematics/systproviders/ValenciaExc2p2hReweighter_tool.hh"
 
 #include "fhiclcpp/ParameterSet.h"
 
@@ -80,6 +81,8 @@ make_instance(fhicl::ParameterSet const &paramset) {
     return std::make_unique<QEInterference>(paramset);
   } else if (tool_type == "MECq0q3InterpWeighting") {
     return std::make_unique<MECq0q3InterpWeighting>(paramset);
+  } else if (tool_type == "ValenciaExc2p2hReweighter") {
+    return std::make_unique<ValenciaExc2p2hReweighter>(paramset);
   } else {
     throw unknown_nusyst_systprovider()
         << "[ERROR]: Unknown tool type: " << std::quoted(tool_type);
